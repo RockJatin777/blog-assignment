34673972b2c67cad75e55a5e9ec31cc3d468f08f
RockJatin777 <rockjatin0748@gmail.com>
2024-06-27 19:43:59 +0530

    Update index.js

--- src/Component/BlogList/index.js
+++ src/Component/BlogList/index.js
@@ -33,7 +33,7 @@ const Bloglist = () => {
     const [isErr, setIsErr] = useState(false)
     const [isVisible, setIsVisible] = useState(false)
     const [errMsg, setErrMsg] = useState('')
-    const [blogs, setBlogs] = useState(bloglist)
+    const [blogs, setBlogs] = useState(JSON.parse(localStorage.getItem('blogs')))
 
     // adding author details of blog from the user
     const onAddAuthor = event => {
